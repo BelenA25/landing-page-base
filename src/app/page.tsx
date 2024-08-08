526e0f2bc150fa4cfcf03bd6658e8f85a68feedd
Belencita <beckiar23@gmail.com>
2024-08-08 18:28:58 -0400

    The component button was created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import { TypographyH2 } from "@/components/typography";
+import { TypographyH2, TypographyP } from "@/components/typography";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <TypographyH2>This is the first tittle</TypographyH2>
+      <TypographyP>This is a paragraph</TypographyP>
+      <div className="flex justify-center my-8">
+        <Image src="/assets/computer.png" alt="Computer" width={400} height={200} className="object-contain" />
+      </div>
     </>
   );
 }
