d2b407679d2bd406fa55f404692cc24817ad2bd4
Belencita <beckiar23@gmail.com>
2024-08-12 09:39:36 -0400

    The testimonials section was improved with supabase data

--- src/components/testimonialSection.tsx
+++ src/components/testimonialSection.tsx
@@ -1,7 +1,15 @@
-import { Carousel, CarouselContent, CarouselItem, CarouselPrevious, CarouselNext } from "@/components/ui/carousel";
+
+import { createClient } from '@/utils/supabase/server'; import { Carousel, CarouselContent, CarouselItem, CarouselPrevious, CarouselNext } from "@/components/ui/carousel";
 import { TypographyH2, TypographyP } from "./ui/typography";
 
-export default function Testimonials() {
+export default async function Testimonials() {
+    const supabase = createClient();
+    const { data: testimonials } = await supabase.from("Testimonial").select();
+
+    if (!testimonials) {
+        return <p>No testimonials found</p>;
+    }
+
     return (
         <section className="my-12 mx-8">
             <div className="flex items-center justify-between max-w-screen-lg mx-auto">
@@ -12,20 +20,18 @@ export default function Testimonials() {
                 </div>
                 <div className="flex-1 relative ml-8">
                     <Carousel className="relative">
-                        <CarouselPrevious className="absolute z-10  left-0 top-1/2 transform -translate-y-1/2" />
+                        <CarouselPrevious className="absolute z-10 left-0 top-1/2 transform -translate-y-1/2" />
                         <CarouselContent>
-                            <CarouselItem className="p-8 border rounded-lg shadow-md">
-                                <TypographyP alignment="center">"This product changed my life. I recommend it 100%!"</TypographyP>
-                                <TypographyP alignment="center" fontWeight="semibold">- Client 1</TypographyP>
-                            </CarouselItem>
-                            <CarouselItem className="p-8 border rounded-lg shadow-md">
-                                <TypographyP alignment="center">"The customer service was exceptional."</TypographyP>
-                                <TypographyP alignment="center" fontWeight="semibold">- Client 2</TypographyP>
-                            </CarouselItem>
-                            <CarouselItem className="p-8 border rounded-lg shadow-md">
-                                <TypographyP alignment="center">"Great quality and fast delivery!"</TypographyP>
-                                <TypographyP alignment="center" fontWeight="semibold">- Client 3</TypographyP>
-                            </CarouselItem>
+                            {testimonials.map((testimonial, index) => (
+                                <CarouselItem key={index} className="p-8 border rounded-lg shadow-md">
+                                    <TypographyP alignment="center">
+                                        "{testimonial.clientTestimonial}"
+                                    </TypographyP>
+                                    <TypographyP alignment="center" fontWeight="semibold">
+                                        - {testimonial.clientName}
+                                    </TypographyP>
+                                </CarouselItem>
+                            ))}
                         </CarouselContent>
                         <CarouselNext className="absolute right-0 top-1/2 transform -translate-y-1/2" />
                     </Carousel>
