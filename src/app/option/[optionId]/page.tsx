eaa7e84f3c1cfb0f888a93453eeb49e28fffbf94
Belencita <beckiar23@gmail.com>
2024-08-15 17:04:50 -0400

    Open graph was configured on product page

--- src/app/option/[optionId]/page.tsx
+++ src/app/option/[optionId]/page.tsx
@@ -1,5 +1,6 @@
 import Typography from "@/components/Typography/typography";
 import { createClient } from "@/utils/supabase/server";
+import Head from "next/head";
 import Image from "next/image";
 
 export default async function Option({ params }: { params: { optionId: string } }) {
@@ -20,13 +21,21 @@ export default async function Option({ params }: { params: { optionId: string } 
     }
 
     return (
-        <div className="p-6 max-w-screen-md mx-auto">
-            <div className="flex justify-center items-center mx-5 my-5">
-                <Image width={400} height={200} src={productData.image_link} alt={""} className="" />
-            </div>
+        <>
+            <Head>
+                <title>{productData.name}</title>
+                <meta property="og:title" content={productData.name} />
+                <meta property="og:description" content={productData.description} />
+                <meta property="og:image" content={productData.image_link} />
+            </Head>
+            <div className="p-6 max-w-screen-md mx-auto">
+                <div className="flex justify-center items-center mx-5 my-5">
+                    <Image width={400} height={200} src={productData.image_link} alt={""} className="" />
+                </div>
 
-            <Typography tag="h1">{productData.name}</Typography>
-            <Typography tag="p">{productData.description}</Typography>
-        </div>
+                <Typography tag="h1">{productData.name}</Typography>
+                <Typography tag="p">{productData.description}</Typography>
+            </div>
+        </>
     );
 }
