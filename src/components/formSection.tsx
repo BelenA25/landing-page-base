d60a9a9bedc24575125e8c9e617f6456b9e4336e
Belencita <beckiar23@gmail.com>
2024-08-16 12:02:33 -0400

    The form section was improved by allowing data to be uploaded

--- src/components/formSection.tsx
+++ src/components/formSection.tsx
@@ -6,6 +6,7 @@ import { z } from "zod"
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import { createClient } from '@/utils/supabase/client';
 
 const formSchema = z.object({
     name: z.string().min(2, "Name must be at least 2 characters long").max(50, "Name cannot exceed 50 characters"),
@@ -14,45 +15,58 @@ const formSchema = z.object({
 })
 
 export function ContactForm() {
+    const supabase = createClient();
     const form = useForm({
         resolver: zodResolver(formSchema),
     });
     const onSubmit = async (data: any) => {
         console.log(data);
+
+        const { error } = await supabase
+            .from('Client')
+            .insert([
+                { name: data.name, email: data.email, phoneNumber: data.phone }
+            ]);
+
+        if (error) {
+            console.error("Error inserting data:", error.message);
+        } else {
+            console.log("Data successfully inserted!");
+        }
     };
 
     return (
         <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 max-w-lg mx-auto my-9">
                 <FormField control={form.control} name="name" render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Name</FormLabel>
-                            <FormControl>
-                                <Input placeholder="John Smith" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
+                    <FormItem>
+                        <FormLabel>Name</FormLabel>
+                        <FormControl>
+                            <Input placeholder="John Smith" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                    </FormItem>
+                )}
                 />
                 <FormField control={form.control} name="email" render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Email</FormLabel>
-                            <FormControl>
-                                <Input placeholder="example@email.com" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
+                    <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                            <Input placeholder="example@email.com" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                    </FormItem>
+                )}
                 />
                 <FormField control={form.control} name="phone" render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Phone number</FormLabel>
-                            <FormControl>
-                                <Input placeholder="(+591) XXXXXX" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
+                    <FormItem>
+                        <FormLabel>Phone number</FormLabel>
+                        <FormControl>
+                            <Input placeholder="(+591) XXXXXX" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                    </FormItem>
+                )}
                 />
                 <Button type="submit" className="w-full">Submit</Button>
             </form>
