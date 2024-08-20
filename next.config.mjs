5d2482e42b9b2ed3de9c7f47231379ca265230e8
Belencita <beckiar23@gmail.com>
2024-08-20 15:04:32 -0400

    The Content Security Policy was added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,40 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { images:{domains:['cdn.pixabay.com', 'freesvg.org', 'upload.wikimedia.org', 'logos-world.net']}};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: [
+        'cdn.pixabay.com',
+        'freesvg.org',
+        'upload.wikimedia.org',
+        'logos-world.net',
+      ],
+    },
+    async headers() {
+      const cspHeader = `
+        default-src 'self';
+        script-src 'self' 'unsafe-eval' 'unsafe-inline';
+        style-src 'self' 'unsafe-inline';
+        img-src 'self' blob: data:;
+        font-src 'self';
+        object-src 'none';
+        base-uri 'self';
+        form-action 'self';
+        frame-ancestors 'none';
+        upgrade-insecure-requests;
+      `;
+  
+      return [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'Content-Security-Policy',
+              value: cspHeader.replace(/\n/g, ''),
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
